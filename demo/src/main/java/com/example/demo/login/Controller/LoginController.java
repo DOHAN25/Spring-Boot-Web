a182c5530abcdd336b8dd85283f6199be0d31643
githun DOHAN25 <joke3323@naver.com>
2022-07-24 21:43:03 +0900

    login update

--- demo/src/main/java/com/example/demo/login/Controller/LoginController.java
+++ demo/src/main/java/com/example/demo/login/Controller/LoginController.java
@@ -1,8 +1,25 @@
 package com.example.demo.login.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.example.demo.user.vo.UserVO;
+
+import javax.servlet.http.HttpSession;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Controller
 public class LoginController {
 	
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+	@RequestMapping(value="login.do", method=RequestMethod.POST)
+	public String loginDo(HttpSession session, @RequestBody UserVO userInfo) {
+		logger.info("[LoginController] : loginStart");
+		return "";
+	}
 }
+ 
